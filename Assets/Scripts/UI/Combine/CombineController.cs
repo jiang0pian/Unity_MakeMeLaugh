36edb912bc10a83239d56b9070a5fb3853042a7a
jiang0pian <121656611+jiang0pian@users.noreply.github.com>
2024-01-26 19:57:01 +0800

    jky

---
+++ Assets/Scripts/UI/Combine/CombineController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombineController : MonoBehaviour
+{
+    public List<string> combineList;
+    public List<Slot> combineSlots;
+}
