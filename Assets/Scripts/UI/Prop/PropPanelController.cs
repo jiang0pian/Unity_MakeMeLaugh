2e99cfcab4a917d2a4c57ec5bfa59792132e7bf1
jiang0pian <121656611+jiang0pian@users.noreply.github.com>
2024-01-28 00:23:43 +0800

    UseProp

--- Assets/Scripts/UI/Prop/PropPanelController.cs
+++ Assets/Scripts/UI/Prop/PropPanelController.cs
@@ -10,12 +10,52 @@ public class PropPanelController : MonoBehaviour
     {
         Instance = this;
     }
+    private void Update()
+    {
+        
+    }
 
     public void RefreshAllPropSlot()
     {
         foreach(var slot in PropSlots)
         {
             slot.RefreshBagSlotInfo();
+        }
+    }
+
+    public void getKeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && PropSlots[0].isContainedItem==true)
+        {
+            PropSlots[0].GetComponent<Prop>().UseProp();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) && PropSlots[1].isContainedItem == true)
+        {
+            PropSlots[1].GetComponent<Prop>().UseProp();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) && PropSlots[2].isContainedItem == true)
+        {
+            PropSlots[2].GetComponent<Prop>().UseProp();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) && PropSlots[3].isContainedItem == true)
+        {
+            PropSlots[3].GetComponent<Prop>().UseProp();
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && PropSlots[4].isContainedItem == true)
+        {
+            PropSlots[4].GetComponent<Prop>().UseProp();
+        }
+        if (Input.GetKeyDown(KeyCode.E) && PropSlots[5].isContainedItem == true)
+        {
+            PropSlots[5].GetComponent<Prop>().UseProp();
+        }
+        if (Input.GetKeyDown(KeyCode.R) && PropSlots[6].isContainedItem == true)
+        {
+            PropSlots[6].GetComponent<Prop>().UseProp();
+        }
+        if (Input.GetKeyDown(KeyCode.F) && PropSlots[7].isContainedItem == true)
+        {
+            PropSlots[7].GetComponent<Prop>().UseProp();
         }
     }
 }
