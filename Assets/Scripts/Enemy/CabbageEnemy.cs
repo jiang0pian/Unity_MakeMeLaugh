d78277141a608f035f1a67647213169b9274e1e0
3025361190 <119148001+3025361190@users.noreply.github.com>
2024-01-27 21:02:41 +0800

    青菜怪AI及其预制体

--- Assets/Scripts/Enemy/CabbageEnemy.cs
+++ Assets/Scripts/Enemy/CabbageEnemy.cs
@@ -8,7 +8,11 @@ public class CabbageEnemy : MonoBehaviour
 
     public float maxHealth;
     public float currentHealth;
-    public float moveSpeed;
+    public float jumpForce = 50f;
+
+    public float jumpDurationTime = 2f;
+
+    private int jumpNum = 0;
 
     private bool isFindPlayer;
 
@@ -25,7 +29,50 @@ public class CabbageEnemy : MonoBehaviour
         lookDirection = new Vector2(1, 0);
     }
 
+    private void Start()
+    {
+        StartCoroutine(jump());
+    }
 
+    private void Update()
+    {
+        if (pursuitRange.OverlapPoint(PlayerController.Instance.transform.position))
+        {
+            isFindPlayer = true;
+        }
+        if(isFindPlayer == true)
+        {
+            if (transform.position.x < PlayerController.Instance.transform.position.x)
+            {
+                lookDirection.x = 1;
+                enemySprite.transform.localScale = new Vector3(lookDirection.x, enemySprite.transform.localScale.y, enemySprite.transform.localScale.z);
+
+            }
+            else
+            {
+                lookDirection.x = -1;
+                enemySprite.transform.localScale = new Vector3(lookDirection.x, enemySprite.transform.localScale.y, enemySprite.transform.localScale.z);
+
+            }
+        }
+    }
+
+    public IEnumerator jump()
+    {
+        if(isFindPlayer == false)
+        {
+            if (jumpNum > 1)
+            {
+                lookDirection = -lookDirection;
+                enemySprite.transform.localScale = new Vector3(lookDirection.x, enemySprite.transform.localScale.y, enemySprite.transform.localScale.z);
+                jumpNum = 0;
+            }
+            jumpNum++;
+        }
+        rigidbody2D.AddForce(new Vector2(lookDirection.x, 3) * jumpForce, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(jumpDurationTime);
+        StartCoroutine(jump());
+    }
 
     public void ChangeHealth(float damage)
     {
@@ -37,6 +84,15 @@ public class CabbageEnemy : MonoBehaviour
         else if (currentHealth > maxHealth)
         {
             currentHealth = maxHealth;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(GetComponent<PhysicsCheck>().isOnGround == true && attackRange.OverlapPoint(PlayerController.Instance.transform.position))
+        {
+            //对玩家造成伤害...................
+            Debug.Log("对玩家造成了伤害");
         }
     }
 
