faa1fb4b8ef4da797ab11535bd4021a995706344
xiaotu1234 <157554741+xiaotu1234@users.noreply.github.com>
2024-01-27 22:30:59 +0800

    饼干碎裂动画

--- Assets/Scripts/map/cookies.cs
+++ Assets/Scripts/map/cookies.cs
@@ -8,18 +8,23 @@ public class cookies : MonoBehaviour
     public Collider2D door; // 饼干的碰撞体
     public bool isCookiePressd =false; // 饼干是否被碰到
     public Animator animator;
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 private void OnCollisionEnter2D(Collision2D collision)
 {
     Debug.Log(collision.gameObject.tag);
     if (collision.gameObject.CompareTag("Player"))
     {
-        StartCoroutine(DelayedExecution());
+            animator.SetBool("suilie", true);
+            StartCoroutine(DelayedExecution());
         }
 }
 
 IEnumerator DelayedExecution()
 {
-    yield return new WaitForSeconds(3.0f);
+    yield return new WaitForSeconds(1.0f);
     isCookiePressd = true;
 }
 
