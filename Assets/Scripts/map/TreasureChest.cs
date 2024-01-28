16a11507bccbd34185fa6fd15eae0479d0452d10
jiang0pian <121656611+jiang0pian@users.noreply.github.com>
2024-01-28 12:42:54 +0800

    1

---
+++ Assets/Scripts/map/TreasureChest.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour
+{
+    public int radomAwardAmount;
+    public List<int> appointAwardAmountList;
+    public bool isGetRadom;
+
+    public void GetRadomAward()
+    {
+        InventoryManager.Instance.GetRadomProp(radomAwardAmount);
+    }
+    public void GetAppointAward()
+    {
+        for(int i = 0; i < appointAwardAmountList.Count; i++)
+        {
+            InventoryManager.Instance.GetProp(appointAwardAmountList[i], i);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (isGetRadom)
+            {
+                GetRadomAward();
+            }
+            else
+            {
+                GetAppointAward();
+            }
+            Destroy(this.gameObject);
+        }
+    }
+
+}
