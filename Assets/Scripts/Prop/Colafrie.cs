67b84711919ac908affdbf8bbc0352c9a17c334c
jiang0pian <121656611+jiang0pian@users.noreply.github.com>
2024-01-28 14:32:26 +0800

    1

---
+++ Assets/Scripts/Prop/Colafrie.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colafrie : Prop
+{
+    public override void UseProp()
+    {
+        PlayerController.Instance.isFrenchfrie = true;
+        StartCoroutine(Frenchfrie());
+    }
+
+    public IEnumerator Frenchfrie()
+    {
+        yield return new WaitForSeconds(0.5f);
+        PlayerController.Instance.isFrenchfrie = false;
+    }
+}
